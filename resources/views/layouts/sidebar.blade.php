8763fb74b7d4dad793ffccb87b130a4454e8e953
safwanrazali <a.safwanrazali@gmail.com>
2026-08-04 15:08:41 +0800

    Update sidebar layout and styling; enhance section titles and logo attributes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,15 +1,12 @@
 <div class="sidebar" id="sidebar">
 
     <div class="sidebar-logo">
-
         <a href="/" class="sidebar-brand">
-            <img src="{{ asset('image/main_logo.png') }}" alt="Logo" class="logo">
+            <img src="{{ asset('image/main_logo.png') }}" alt="SPARK" class="logo">
         </a>
-
         <button class="sidebar-toggle" id="toggleSidebar" aria-label="Toggle sidebar">
             <i class="bi bi-list"></i>
         </button>
-
     </div>
 
     <ul class="sidebar-menu">
@@ -17,56 +14,96 @@
         <li>
             <a href="/">
                 <i class="bi bi-grid"></i>
-                <span class="menu-text">
-                    Papan Pemuka
-                </span>
+                <span class="menu-text">Papan Pemuka</span>
             </a>
         </li>
+
+        <li class="sidebar-section-title"><span class="menu-text">Inventori</span></li>
 
         <li>
             <a href="{{ route('muat-naik.index') }}">
                 <i class="bi bi-upload"></i>
-                <span class="menu-text">
-                    Muat Naik Data
-                </span>
+                <span class="menu-text">Muat Naik MasterTable</span>
             </a>
         </li>
 
         <li>
             <a href="{{ route('muat-naik.history') }}">
                 <i class="bi bi-clock-history"></i>
-                <span class="menu-text">
-                    Sejarah Muat Naik
-                </span>
+                <span class="menu-text">Sejarah Muat Naik</span>
             </a>
         </li>
 
         <li>
-            <a href="#">
-                <i class="bi bi-bar-chart"></i>
-                <span class="menu-text">
-                    Analisis Risiko
-                </span>
+            <a href="#" class="disabled-link" title="Akan datang">
+                <i class="bi bi-hdd-stack"></i>
+                <span class="menu-text">Inventori Kriptografi</span>
+            </a>
+        </li>
+
+        <li class="sidebar-section-title"><span class="menu-text">Penilaian Risiko</span></li>
+
+        <li>
+            <a href="#" class="disabled-link" title="Akan datang">
+                <i class="bi bi-shield-exclamation"></i>
+                <span class="menu-text">Penilaian Risiko</span>
             </a>
         </li>
 
         <li>
-            <a href="#">
-                <i class="bi bi-file-earmark-pdf"></i>
-                <span class="menu-text">
-                    Laporan
-                </span>
+            <a href="#" class="disabled-link" title="Akan datang">
+                <i class="bi bi-journal-text"></i>
+                <span class="menu-text">Sejarah Penilaian</span>
+            </a>
+        </li>
+
+        <li class="sidebar-section-title"><span class="menu-text">Laporan</span></li>
+
+        <li>
+            <a href="#" class="disabled-link" title="Akan datang">
+                <i class="bi bi-file-earmark-bar-graph"></i>
+                <span class="menu-text">Laporan Inventori</span>
             </a>
         </li>
 
         <li>
-            <a href="#">
-                <i class="bi bi-gear"></i>
-                <span class="menu-text">
-                    Tetapan
-                </span>
+            <a href="#" class="disabled-link" title="Akan datang">
+                <i class="bi bi-file-earmark-medical"></i>
+                <span class="menu-text">Laporan Risiko</span>
             </a>
         </li>
+
+        <li>
+            <a href="#" class="disabled-link" title="Akan datang">
+                <i class="bi bi-file-earmark-richtext"></i>
+                <span class="menu-text">Laporan Menyeluruh</span>
+            </a>
+        </li>
+
+        @can('access-administration')
+            <li class="sidebar-section-title"><span class="menu-text">Pentadbiran</span></li>
+
+            <li>
+                <a href="#" class="disabled-link" title="Akan datang">
+                    <i class="bi bi-people"></i>
+                    <span class="menu-text">Pengguna</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#" class="disabled-link" title="Akan datang">
+                    <i class="bi bi-key"></i>
+                    <span class="menu-text">Peranan</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#" class="disabled-link" title="Akan datang">
+                    <i class="bi bi-gear"></i>
+                    <span class="menu-text">Tetapan</span>
+                </a>
+            </li>
+        @endcan
 
     </ul>
 
